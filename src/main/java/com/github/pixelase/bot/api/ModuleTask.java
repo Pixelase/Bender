bde70daa0f606a60a9c181891c0bf236c2c2e103
Alexander Babai <sustem34@gmail.com>
2015-11-02 02:16:10 +0300

    More abstraction in ModuleTask
    Or reflection magic  ¯\_(ツ)_/¯
    
    Important notice:
    * For now, when you'll extend ModuleTask, you must only override 'Run' method
      and invoke there 'startExecution' with your concrete UserTask;

--- src/main/java/com/github/pixelase/bot/api/ModuleTask.java
+++ src/main/java/com/github/pixelase/bot/api/ModuleTask.java
@@ -1,9 +1,12 @@
 package com.github.pixelase.bot.api;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import com.pengrad.telegrambot.model.User;
 
 public abstract class ModuleTask extends Task {
 	protected List<UserTask> userTasks;
@@ -26,5 +29,60 @@ public abstract class ModuleTask extends Task {
 
 	public static void setModuleTaskTimeout(long moduleTaskTimeout) {
 		ModuleTask.moduleTaskTimeout = moduleTaskTimeout;
+	}
+
+	protected void startExecution(Class<? extends UserTask> userTaskClass) throws InstantiationException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		/*
+		 * Temp variable
+		 */
+		UserTask currentUserTask = null;
+
+		while (isOk) {
+			/*
+			 * Fix multithreading
+			 */
+			sleep(moduleTaskTimeout);
+
+			/*
+			 * If message is null we can't get it fields, so skip this
+			 * iteration;
+			 */
+			if (message == null) {
+				continue;
+			}
+
+			/*
+			 * Create currentUserTask with some user from message;
+			 */
+			currentUserTask = userTaskClass.getConstructor(User.class).newInstance(message.from());
+
+			/*
+			 * If the task for a given user is already on our list, skip this
+			 * iteration;
+			 */
+			if (userTasks.contains(currentUserTask)) {
+				continue;
+			}
+
+			/*
+			 * Add task for a given user into our TaskList TODO we can use stack
+			 * for this may be
+			 */
+			userTasks.add(currentUserTask);
+
+			/*
+			 * Update message field for all task's
+			 */
+			for (UserTask userTask : userTasks) {
+				userTask.setMessage(message);
+				userTask.setOk(isOk);
+			}
+
+			/*
+			 * Add task to the executor
+			 */
+			userTaskExecutor.submit(userTasks.get(userTasks.size() - 1));
+		}
 	}
 }
--- src/main/java/com/github/pixelase/bot/modules/weather/WeatherModuleTask.java
+++ src/main/java/com/github/pixelase/bot/modules/weather/WeatherModuleTask.java
@@ -1,7 +1,8 @@
 package com.github.pixelase.bot.modules.weather;
 
+import java.lang.reflect.InvocationTargetException;
+
 import com.github.pixelase.bot.api.ModuleTask;
-import com.github.pixelase.bot.api.UserTask;
 
 public class WeatherModuleTask extends ModuleTask {
 
@@ -11,56 +12,12 @@ public class WeatherModuleTask extends ModuleTask {
 
 	@Override
 	public void run() {
-		/*
-		 * Temp variable
-		 */
-		UserTask currentUserTask = null;
-
-		while (isOk) {
-			/*
-			 * Fix multithreading
-			 */
-			sleep(moduleTaskTimeout);
-
-			/*
-			 * If message is null we can't get it fields, so skip this
-			 * iteration;
-			 */
-			if (message == null) {
-				continue;
-			}
-
-			/*
-			 * Create currentUserTask with some user from message;
-			 */
-			currentUserTask = new WeatherUserTask(message.from());
-
-			/*
-			 * If the task for a given user is already on our list, skip this
-			 * iteration;
-			 */
-			if (userTasks.contains(currentUserTask)) {
-				continue;
-			}
-
-			/*
-			 * Add task for a given user into our TaskList TODO we can use stack
-			 * for this may be
-			 */
-			userTasks.add(currentUserTask);
-
-			/*
-			 * Update message field for all task's
-			 */
-			for (UserTask userTask : userTasks) {
-				userTask.setMessage(message);
-				userTask.setOk(isOk);
-			}
-
-			/*
-			 * Add task to the executor
-			 */
-			userTaskExecutor.submit(userTasks.get(userTasks.size() - 1));
+		try {
+			startExecution(WeatherUserTask.class);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
